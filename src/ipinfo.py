3b1f9f8442fc98ed2078dde6ed3fb28acd229a59
= <cyrus.z.xu@gmail.com>
2021-07-26 19:34:35 -0700

    Added basic ipinfo API request call

---
+++ src/ipinfo.py
@@ -0,0 +1,19 @@
+import sys
+import os
+import json
+import socket
+import requests
+
+sys.path.append(os.getcwd())
+from privatekeys.api_keys import apikeys
+
+hostname = socket.gethostname()
+ipa = socket.gethostbyname(hostname)
+print(hostname + "  " + ipa)
+
+baseurl = "https://ipinfo.io?token="
+fullurl = baseurl + apikeys['ipinfo_key']
+response = requests.get(fullurl)
+x = response.json()
+for i in x:
+    print(i + " , " + x[i])
